78b7d6afd1bd2bd26ad0b4c6ffc805cee163704b
AlexanderNino <wilson.nino02@uptc.edu.co>
2026-05-12 11:18:28 -0500

    EV-251-Corregir-conexión-base-de-datos-APIREST-VISA

--- app/database.py
+++ app/database.py
@@ -1,18 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = "postgresql://visa_user:visa123@localhost:5432/visa_db"
 
 engine = create_engine(DATABASE_URL)
 
