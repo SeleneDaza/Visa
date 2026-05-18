1938b807b0bc34ce312473aed30159ac0c9ada40
AlexanderNino <wilson.nino02@uptc.edu.co>
2026-05-18 17:58:23 -0500

    EV-266-Refactorizar-a-handler.py-VISA-SERVICE

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import visa
-from app.database import engine, Base
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Servicio Visa", version="1.0.0")
-
-app.include_router(visa.router, prefix="/visa", tags=["Visa"])
